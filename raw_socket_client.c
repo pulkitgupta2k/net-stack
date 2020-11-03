7bbc80bac215047b097673aba610797fc0034ec5
Pulkit Gupta <pulkitugpta2k@gmail.com>
2020-11-03 12:32:42 -0500

    readFile func added in client

--- raw_socket_client.c
+++ raw_socket_client.c
@@ -54,14 +54,45 @@ int create_socket(char *device){
 	return sock_fd;
 }
 
+void addByte(BYTE dest[], BYTE src[], int i, int s){
+	int ctr = 0;
+	while(ctr!=s)
+		dest[i++] = src[ctr++];
+}
+
+void readFile(char fileName[], BYTE data[]){
+	FILE *fp;
+	fp = fopen(fileName, "rb");
+	char ch;
+	if (fp == NULL){
+		perror("ERROR while opening the file \n");
+		exit(EXIT_FAILURE);
+	}
+	int i=0;
+	while((ch = fgetc(fp)) != EOF)
+		data[i++] = (BYTE)ch;
+	data[i] = 0x00;
+	fclose(fp);
+	return;
+}
 
 
 int main(int argc, char ** argv){
 	int sock_fd =0;
-	//dst_mac[6] = {0x08, 0x00, 0x27, 0xc5, 0x0d, 0x1c};
-	//src_mac[6] = {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26};
-	//type[2] = {0x88, 0x98};
-	BYTE data[150] = {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26, 0x08, 0x00, 0x27, 0xc5, 0x0d, 0x1c, 0x88, 0x98};
+	BYTE dst_mac[6] = {0x08, 0x00, 0x27, 0xc5, 0x0d, 0x1c};
+	BYTE src_mac[6] = {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26};
+	BYTE type[2] = {0x88, 0x98};
+	BYTE data[150];
+	BYTE fileData[130];
+	readFile("test.txt", fileData);
+	addByte(data, dst_mac, 0, 6);
+	addByte(data, src_mac, 6, 6);
+	addByte(data, type, 12, 2);
+	addByte(data, fileData,14, 130);
+	//strncat(data, dst_mac, 6);
+	//strncat(data, src_mac, 6);
+	//strncat(data, dst_mac, 2);
+	//BYTE data[150] = {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26, 0x08, 0x00, 0x27, 0xc5, 0x0d, 0x1c, 0x88, 0x98};
 	sock_fd = create_socket(argv[1]);
 	if( !(sock_fd)){
 		printf("No SOCK FOUND\n");

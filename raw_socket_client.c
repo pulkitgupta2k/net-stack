4eb640a2a0625e8e6241a9771e90beebca1638a4
Pulkit Gupta <pulkitugpta2k@gmail.com>
2020-11-11 12:36:20 -0500

    sending in chunks

--- raw_socket_client.c
+++ raw_socket_client.c
@@ -42,6 +42,7 @@ void readFile(char fileName[], BYTE data[]){
 		exit(EXIT_FAILURE);
 	}
 	int i=0;
+	memset(data, 0, 150);
 	while((ch = fgetc(fp)) != EOF)
 		data[i++] = (BYTE)ch;
 	data[i] = 0x00;
@@ -49,35 +50,45 @@ void readFile(char fileName[], BYTE data[]){
 	return;
 }
 
+void senderWrapper(int sock, struct fileProtocolWrapper *fpw, char fileName[]){
+	FILE *file = NULL;
+	BYTE data[150];
+	size_t bytesRead=0;	
+
+	file = fopen(fileName, "rb");
+
+	if (file != NULL){
+		while( (bytesRead = fread(fpw->fileData->data, 1,130, file)) >0 ){
+			fpw->fileData->seq++;
+			parseProtocol(fpw, data);
+			write(sock, data, 150);
+		}	
+	}
+	fclose(file);
+	return;
+}
 
 int main(int argc, char ** argv){
 	int sock_fd =0;
 	struct fileProtocol fp;
 
-	fp.seq = 10000000;
+	fp.seq = 0;
 	fp.isEnd = 1;
-	readFile("test.txt", fp.data);	
+	//readFile("test.txt", fp.data);	
 	
 	struct fileProtocolWrapper fpw ={ {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26}, 
 					{0x08, 0x00, 0x27, 0x5c, 0x65, 0x26}, 
 					{0x88, 0x98}, 
 					&fp};
 	
-	BYTE data[150];
-	parseProtocol(&fpw, data);
+	//BYTE data[150];
+	//parseProtocol(&fpw, data);
 
-	//addByte(data, dst_mac, 0, 6);
-	//addByte(data, src_mac, 6, 6);
-	//addByte(data, type, 12, 2);
-	//addByte(data, fileData,14, 130);
-	//strncat(data, dst_mac, 6);
-	//strncat(data, src_mac, 6);
-	//strncat(data, dst_mac, 2);
-	//BYTE data[150] = {0x08, 0x00, 0x27, 0x5c, 0x65, 0x26, 0x08, 0x00, 0x27, 0xc5, 0x0d, 0x1c, 0x88, 0x98};
 	sock_fd = create_socket(argv[1]);
 	if( !(sock_fd)){
 		printf("No SOCK FOUND\n");
 		return 0;
 	}
-	write(sock_fd, data, 150);
+	senderWrapper(sock_fd, &fpw, "test.txt");
+	//write(sock_fd, data, 150);
 }
